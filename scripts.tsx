32072d0e23fc98fe4a2f1d9b915152202da83594
ofk <ofkjpn+github@gmail.com>
2020-06-04 11:51:53 +0900

    Add start script and example files

---
+++ scripts.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root: React.FC = () => {
+  return <p>sandbox</p>;
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
